c68b3f20b6106b7c0cc9e6ed7ebbf546f8e290c2
Emmanuel Ezenwigbo <emmanuelezenwigbo@gmail.com>
2020-09-19 18:55:19 +0100

    Bug Fixes and UI pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   // Transition effect for navbar 
   var $nav = $('.navbar');
   var $win = $(window);
-  var winH = '20';   // Get the window height.
+  var winH = '10';   // Get the window height.
 
   $win.on("scroll", function () {
       if ($(this).scrollTop() > winH ) {
