b0a53e2d0fe8d9fc426f807ca5d4f9e178a5f4b3
Emmanuel Ezenwigbo <emmanuelezenwigbo@gmail.com>
2020-10-20 16:49:40 +0100

    Fixes

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,61 @@
-$(document).ready(function() {
-  // Transition effect for navbar 
+$(document).ready(function () {
+  // Transition effect for navbar
   var $nav = $('.navbar');
   var $win = $(window);
-  var winH = '10';   // Get the window height.
+  var winH = '10'; // Get the window height.
 
-  $win.on("scroll", function () {
-      if ($(this).scrollTop() > winH ) {
-          $nav.addClass("solid");
+  $win
+    .on('scroll', function () {
+      if ($(this).scrollTop() > '20') {
+        $nav.addClass('solid');
       } else {
-          $nav.removeClass("solid");
+        $nav.removeClass('solid');
       }
-  }).on("resize", function(){ // If the user resizes the window
-     winH = $(this).height(); // you'll need the new height value
+    })
+    .on('resize', function () {
+      // If the user resizes the window
+      winH = $(this).height(); // you'll need the new height value
+    });
+
+  $('.carousel-2').slick({
+    autoplay:true,
+    mobileFirst:true,
+    responsive: [
+        {
+            breakpoint: 1200,
+            settings: "unslick",
+        },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          dots: true,
+          infinite: true,
+          speed: 1200,
+          autoplay: true,
+        },
+      },
+      {
+        breakpoint: 300,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: true,
+          infinite: true,
+          speed: 1200,
+          autoplay: true,
+        },
+      },
+    ],
   });
 
+  $('.testimonial-carousel').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    infinite: true,
+    speed: 1000,
+    autoplay: true,
+  });
 });
