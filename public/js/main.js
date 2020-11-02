29a899858ae2bb146a367713a93fefebd2c2c985
Emmanuel Ezenwigbo <emmanuelezenwigbo@gmail.com>
2020-11-02 02:29:57 +0100

    Fixes

--- public/js/main.js
+++ public/js/main.js
@@ -3,6 +3,8 @@ $(document).ready(function () {
   var $nav = $('.navbar');
   var $win = $(window);
   var winH = '10'; // Get the window height.
+  var backButton = '<span class="slick-prev"></span>';
+  var nextButton = '<span class="slick-next"></span>'
 
   $win
     .on('scroll', function () {
@@ -18,37 +20,38 @@ $(document).ready(function () {
     });
 
   $('.carousel-2').slick({
-    autoplay:true,
-    mobileFirst:true,
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    arrows:false,
+    slidesToScroll: 1,
+    autoplay: true,
     responsive: [
-        {
-            breakpoint: 1200,
-            settings: "unslick",
-        },
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 1,
-          dots: true,
           infinite: true,
-          speed: 1200,
-          autoplay: true,
-        },
+          dots: true
+        }
       },
       {
-        breakpoint: 300,
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1,
-          dots: true,
-          infinite: true,
-          speed: 1200,
-          autoplay: true,
-        },
-      },
-    ],
-  });
+          slidesToScroll: 1
+        }
+      }
+    ]});
 
   $('.testimonial-carousel').slick({
     slidesToShow: 1,
@@ -58,4 +61,42 @@ $(document).ready(function () {
     speed: 1000,
     autoplay: true,
   });
+
+  $('.carousel').slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    dots: true,
+    infinite: true,
+    speed: 700,
+    autoplay: true,
+    arrows:true,
+    prevArrow: backButton,
+    nextArrow: nextButton,
+    responsive: [
+      {
+        breakpoint: 1100,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      }
+    ]
+  });
+
 });
